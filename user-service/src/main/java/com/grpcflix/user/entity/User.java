5c6f285f6d8963afe154b7e334a6a757543cc713
kkty39 <yingyitong39@gmail.com>
2023-08-04 23:09:07 -0700

    user + movie services added

---
+++ user-service/src/main/java/com/grpcflix/user/entity/User.java
@@ -0,0 +1,20 @@
+package com.grpcflix.user.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@ToString
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    private String login;
+    private String name;
+    private String genre;
+}
---
+++ user-service/src/main/java/com/grpcflix/user/service/UserService.java
@@ -0,0 +1,48 @@
+package com.grpcflix.user.service;
+
+
+import com.grpcflix.user.repository.UserRepository;
+import com.kkty39.grpcflix.common.Genre;
+import com.kkty39.grpcflix.user.UserGenreUpdateRequest;
+import com.kkty39.grpcflix.user.UserResponse;
+import com.kkty39.grpcflix.user.UserSearchRequest;
+import com.kkty39.grpcflix.user.UserServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@GrpcService
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void getUserGenre(UserSearchRequest request, StreamObserver<UserResponse> responseObserver) {
+        UserResponse.Builder builder = UserResponse.newBuilder();
+        this.userRepository.findById(request.getLoginId())
+                .ifPresent(user -> {
+                    builder.setName(user.getName())
+                            .setLoginId(user.getLogin())
+                            .setGenre(Genre.valueOf(user.getGenre().toUpperCase()));
+                });
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    @Transactional
+    public void updateUserGenre(UserGenreUpdateRequest request, StreamObserver<UserResponse> responseObserver) {
+        UserResponse.Builder builder = UserResponse.newBuilder();
+        this.userRepository.findById(request.getLoginId())
+                .ifPresent(user -> {
+                    user.setGenre(request.getGenre().toString());
+                    builder.setName(user.getName())
+                            .setLoginId(user.getLogin())
+                            .setGenre(Genre.valueOf(user.getGenre().toUpperCase()));
+                });
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+}
